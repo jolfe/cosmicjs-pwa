f1ad5ac45c8da3e79d26c01c23317aacd0cc3f44
Jack Wolfe <jack@brushfire.com>
2021-11-26 18:13:11 -0600

    v1

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,9 @@
 const config = {
-    
-bucket_name: "react-pwa",
-bucket_slug: "react-pwa",
-bucket_id: "5c3f6c46081d3062a56ea573",
-read_key: "d12casi186oW1ZTQcJb2GaCXAeVukJgUPTPT13tAzfpssSWFWU",
-write_key: "eDwoad693AAsC6ppiIE2IWKAlG0n8xPYFpP1QrSCrp0MZ6Lis7",
-url: "https://api.cosmicjs.com/v1/"
-
-}
+  bucket_name: "react-pwa",
+  bucket_slug: "react-pwa",
+  bucket_id: "61a176126ee3e00008eaa500",
+  read_key: "d12casi186oW1ZTQcJb2GaCXAeVukJgUPTPT13tAzfpssSWFWU",
+  write_key: "eDwoad693AAsC6ppiIE2IWKAlG0n8xPYFpP1QrSCrp0MZ6Lis7",
+  url: "https://api.cosmicjs.com/v1/",
+};
 export default config;
